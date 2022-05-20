7be5f541c7f8ebb56b06d1283ee6a8a6f2bc2631
Veselun3007 <fedyshenb@gmail.com>
2022-05-20 23:04:07 +0300

    Fix ErrorPages

--- TaskSchedulerClient/Controllers/HomeController.cs
+++ TaskSchedulerClient/Controllers/HomeController.cs
@@ -22,12 +22,6 @@ namespace TaskSchedulerClient.Controllers
         public IActionResult Privacy()
         {
             return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+        }     
     }
 }
